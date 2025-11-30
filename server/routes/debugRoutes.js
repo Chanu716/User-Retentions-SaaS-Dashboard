27a8b067feb44df6b54d078213bf7313bc07d19c
Chanikya K <karrichanikya@gmail.com>
2025-12-01 02:14:15 +0530

    Remove old project files

--- server/routes/debugRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStats } = require('../controllers/debugController');
-const { apiKeyOrAuth } = require('../middleware/apiKeyOrAuth');
-
-// Protected debug stats (accepts API key or JWT)
-router.get('/stats', apiKeyOrAuth, getStats);
-
-module.exports = router;
